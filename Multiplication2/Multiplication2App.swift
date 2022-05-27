e18d457fbad8d8dae5af41665af011a25fd2c039
Waihon Yew <yewwaihon@gmail.com>
2022-05-28 04:10:21 +0800

    Initial Commit

---
+++ Multiplication2/Multiplication2App.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplication2App.swift
+//  Multiplication2
+//
+//  Created by Waihon Yew on 28/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Multiplication2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
