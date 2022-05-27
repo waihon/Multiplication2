b4ebf83cc624587cbf5577c9109bded477a57290
Waihon Yew <yewwaihon@gmail.com>
2022-05-28 05:20:16 +0800

    Simulate passing data between views

--- Multiplication2/ContentView.swift
+++ Multiplication2/ContentView.swift
@@ -15,19 +15,21 @@ struct GameView: View {
 }
 
 struct SettingsView: View {
-    let callback: () -> Void
+    let callback: (Int, Int) -> Void
 
     var body: some View {
         Text("Settings")
             .padding()
         Button("Start Game") {
-            callback()
+            callback(8, 20)
         }
     }
 }
 
 struct ContentView: View {
     @State private var gameIsActive = false
+    @State private var tablesUpTo = 8
+    @State private var questionCount = 10
 
     var body: some View {
         Text("Content")
@@ -35,8 +37,17 @@ struct ContentView: View {
         if gameIsActive {
             GameView()
         } else {
-            SettingsView(callback: { gameIsActive = true })
+            SettingsView(callback: startGame)
         }
+    }
+
+    func startGame(tablesUpTo: Int, questionCount: Int) {
+        self.tablesUpTo = tablesUpTo
+        self.questionCount = questionCount
+        print(tablesUpTo)
+        print(questionCount)
+
+        gameIsActive = true
     }
 }
 
